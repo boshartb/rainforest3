428daef8a1a5ce60f8667873ce7e2946258d033b
Brant <boshartb@gmail.com>
2015-08-08 23:27:20 -0400

    Adding authorization to ensure users are logged in before they create reviews

--- config/routes.rb
+++ config/routes.rb
@@ -1,24 +1,18 @@
 Rainforest::Application.routes.draw do
-  get 'reviews/show'
-
-  get 'reviews/create'
-
-  get 'reviews/destroy'
-
-  resources :users, only: [:new, :create]
-  resources :sessions, only: [:new, :create, :destroy]
   resources :products do
     resources :reviews, only: [:show, :create, :destroy]
   end
-end
+
+  resources :users, only: [:new, :create]
+  resources :sessions, only: [:new, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
 
+end
   # Example of regular route:
-  #   get 'products/:id' => 'catalog#view'
+     # get 'products/:id' => 'catalog#view'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase
